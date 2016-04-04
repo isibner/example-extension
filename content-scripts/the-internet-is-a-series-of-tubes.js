408ce3617606d05f908ab98c6a20a2e842943b35
Ian Sibner <sibnerian@gmail.com>
2016-04-04 13:28:13 -0400

    fix typo

--- content-scripts/the-internet-is-a-series-of-tubes.js
+++ content-scripts/the-internet-is-a-series-of-tubes.js
@@ -3,7 +3,7 @@
  *  - jQuery's .contents() function is necessary if we want text nodes
  *  - A node has nodeType === 1 if it's an element, === 3 if it's text
  *  - An element node has tagName indicating its tagname (useful for not hitting scripts)
- *  - $.filter operates with the dom node as it's 'this' context
+ *  - $.filter operates with the dom node as its 'this' context
  *  - $.replaceWith can take a function that dictates what text to change,
  *    also with a DOM node 'this' context
 **/
